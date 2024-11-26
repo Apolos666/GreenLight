2506af3103a4cc42b2eb6587193fe247bfc58f69
Apolos666 <quangprox121@gmail.com>
2024-11-26 20:55:59 +0700

    feat: custom json response, better error response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.apolos.net/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		http.NotFound(w, r)
 		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casblance",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
